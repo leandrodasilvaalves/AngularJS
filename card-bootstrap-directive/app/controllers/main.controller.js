ef485c253b7a15b6cb90d30e8b564fac10ce5c82
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-02-12 11:28:01 -0200

    Correção titulo Doc

--- card-bootstrap-directive/app/controllers/main.controller.js
+++ card-bootstrap-directive/app/controllers/main.controller.js
@@ -1,6 +1,6 @@
 app.controller('mainCtrl',function($scope, $rootScope){
 
-    $rootScope.titudoDoc ="Trabalhando com diretivas.";
+    $rootScope.tituloDoc ="Trabalhando com diretivas.";
     $scope.tituloCard="Este é o meu card"
     $scope.descricaoCard="Sua descrição vem direto do scope deste documemto";
 
