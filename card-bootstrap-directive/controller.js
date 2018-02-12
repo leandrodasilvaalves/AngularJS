bc8209abaf538660315b62de61974f4587dbb738
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-02-12 11:13:00 -0200

    Separação do template e configuração do scope da diretiva

--- card-bootstrap-directive/controller.js
+++ card-bootstrap-directive/controller.js
@@ -1,5 +1,7 @@
 app.controller('appCtrl',function($scope, $rootScope){
 
-    $rootScope.titulo ="Trabalhando com diretivas.";
+    $rootScope.titudoDoc ="Trabalhando com diretivas.";
+    $scope.tituloCard="Este é o meu card"
+    $scope.descricaoCard="Sua descrição vem direto do scope deste documemto";
 
 });
--- card-bootstrap-directive/directive.js
+++ card-bootstrap-directive/directive.js
@@ -1,13 +1,11 @@
-app.directive('ngCard',function(){
-    return{
+app.directive('ngCard', function () {
+    return {
+        templateUrl: 'directive.html',
         restrict: 'AE',
-        template:`<div class="card" style="width: 20rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-block">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>`
+        replace: true,
+        transclude: true,
+        scope: {
+            titulo: '@'
+        }
     }
 });
