e833c71e164747f0fbde10ff3160e02c4239a924
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-03-14 13:33:52 -0300

    toogle select e arrow

--- select2-custom-template/app/directives/select2-custom/select2-custom.directive.js
+++ select2-custom-template/app/directives/select2-custom/select2-custom.directive.js
@@ -1,14 +1,22 @@
 angular.module('appSelect2Custom',[]).directive('select2Custom', function(){
 
+
     return{
         retrict:'E',
         replace:true,
         templateUrl:'app/directives/select2-custom/select2-custom.html',
         scope:{
-            clicked:'@'
         },
-        link:function(scope, element, attrs){
-            scope.clicked =false
+        link:function(scope, element, attrs, ctrl){
+            scope.clicked =false;
+            scope.arrowUp =false;
+
+            scope.toogleClick = function(){
+                scope.clicked = !scope.clicked;
+                scope.arrowUp = !scope.arrowUp;
+            };  
+
+            
         }
         
     }
