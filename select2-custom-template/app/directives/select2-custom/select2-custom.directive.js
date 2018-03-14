3e51d02d7abc3016e8d99f3e59b3474e7716824f
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-03-14 12:12:45 -0300

    inicio directive

--- select2-custom-template/app/directives/select2-custom/select2-custom.directive.js
+++ select2-custom-template/app/directives/select2-custom/select2-custom.directive.js
@@ -3,7 +3,13 @@ angular.module('appSelect2Custom',[]).directive('select2Custom', function(){
     return{
         retrict:'E',
         replace:true,
-        templateUrl:'app/directives/select2-custom/select2-custom.html'
+        templateUrl:'app/directives/select2-custom/select2-custom.html',
+        scope:{
+            clicked:'@'
+        },
+        link:function(scope, element, attrs){
+            scope.clicked =false
+        }
         
     }
 });
