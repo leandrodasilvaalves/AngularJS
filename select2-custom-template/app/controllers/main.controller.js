684860619e430f30361d0b77f67782107e5ad8da
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-03-14 16:16:51 -0300

    Selecionando e filtrando registro

--- select2-custom-template/app/controllers/main.controller.js
+++ select2-custom-template/app/controllers/main.controller.js
@@ -1,3 +1,4 @@
 angular.module('appSelect2Custom').controller('mainController',function($scope){
     $scope.nomes=['Leandro','Carlos', 'Eduardo', 'Meiry', 'Isa', 'Gleisson'];
+    $scope.estados = ['Goiás', 'Distrito Federal', 'São Paulo', 'Minas Gerais','Bahia','Pernambuco', 'Paraíba', 'Rio Grande do Norte', 'Mato Grosso do Sul', 'Mato Grosso'];
 });
--- select2-custom-template/app/directives/select2-custom/select2-custom.js
+++ select2-custom-template/app/directives/select2-custom/select2-custom.js
@@ -6,16 +6,25 @@ angular.module('appSelect2Custom').directive('select2Custom', function(){
         replace:true,
         templateUrl:'app/directives/select2-custom/select2-custom.html',
         scope:{
-            array:'=list'
+            array:'=list',
+            placeholder:'@'
         },
         link:function(scope, element, attrs, ctrl){
             scope.clicked =false;
             scope.arrowUp =false;
+            scope.selected ='';
+            scope.placeholder='Selecione...'
 
             scope.toogleClick = function(){
                 scope.clicked = !scope.clicked;
                 scope.arrowUp = !scope.arrowUp;
-            };  
+            }; 
+            
+            scope.setSelected= function(obj){
+                scope.selected = obj;
+                scope.toogleClick();
+                scope.search='';
+            };
         }
         
     }
